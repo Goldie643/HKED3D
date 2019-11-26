ed798a32c4e1826ae2910507e19960e9351859d1
Alex Goldsack <alex.goldsack1@gmail.com>
2019-11-27 01:01:51 +0900

    Added vector functions, tidied vertex vars.

--- hked3d_testing.py
+++ hked3d_testing.py
@@ -3,6 +3,7 @@ import numpy as np
 import math
 from mpl_toolkits.mplot3d import axes3d
 
+# Setting up the default PMTs
 n_rings = 100
 n_per_ring = 400
 
@@ -18,19 +19,35 @@ ys = [hk_r*np.sin(theta) for theta in thetas]
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
 
+# Plot each ring individually
 for height in heights:
     ax.scatter(xs,ys,height,depthshade=0,color="grey",alpha=0.2,s=0.5)
 
-hs = [hk_hh]*n_per_ring
+# Vertex information
+vtx = [0,0,0]
 
-xs = xs[:len(xs)]
-ys = ys[:len(ys)]
-hs = hs[:len(hs)]
+# Direction
+vtx_dir = [1,0,0]
 
-xs.append(0)
-ys.append(0)
-hs.append(0)
+# Energy (testing)
+bse = 50 # MeV
 
-ax.plot_trisurf(xs,ys,hs)
+def dot(v1, v2):
+    return sum([a*b for a,b in zip(v1,v2)])
+
+def length(v):
+    return math.sqrt(dot(v,v))
+
+def angle(v1,v2):
+    return math.acos(dot(v1,v2) / (length(v1) * length(v2)))
+
+vect_scale = bse/5*math.sqrt(dot(vtx_dir,vtx_dir))
+
+vtx_arrow = [vect_scale*a for a in vtx_dir]
+
+# Cone (half) angle
+thet_ch = math.pi/2
+
+ax.quiver(*vtx,*vtx_arrow)
 
 plt.show()
