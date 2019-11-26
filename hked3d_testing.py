f39c94fbb34911baa83de54b07e163849d7ebe2d
Alex Goldsack <alex.goldsack1@gmail.com>
2019-11-27 00:30:57 +0900

    Plotting normal PMT positions.

---
+++ hked3d_testing.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from mpl_toolkits.mplot3d import axes3d
+
+n_rings = 100
+n_per_ring = 400
+
+hk_r = 20
+hk_hh = 20
+
+thetas = np.linspace(0,2*math.pi,n_per_ring)
+heights = np.linspace(-hk_hh,hk_hh,n_rings)
+
+xs = [hk_r*np.cos(theta) for theta in thetas]
+ys = [hk_r*np.sin(theta) for theta in thetas]
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+
+for height in heights:
+    ax.scatter(xs,ys,height,depthshade=0,color="grey",alpha=0.2,s=0.5)
+
+plt.show()
